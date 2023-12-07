0351b5bd235a777c8ddba2799c52bc47c6673e7e
Bruno Rocha <mail.brunorocha@gmail.com>
2023-12-07 08:41:35 -0300

    Initial Commit

---
+++ KleverChallenge/KleverChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  KleverChallengeApp.swift
+//  KleverChallenge
+//
+//  Created by Bruno Rocha on 07/12/23.
+//
+
+import SwiftUI
+
+@main
+struct KleverChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
